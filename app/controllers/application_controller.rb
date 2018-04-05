ee022b6df2bd28f0c83f63ca794241149a6c3aa3
Jay Bednar <ironviralmedia@gmail.com>
2018-04-05 01:32:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ require 'sinatra/base'
 # require '../config/environment.rb'
 
 class App < Sinatra::Base
-    
+
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-      erb :'..views/super_hero'
+      erb :'../views/super_hero'
     end
 
     post'/teams' do
