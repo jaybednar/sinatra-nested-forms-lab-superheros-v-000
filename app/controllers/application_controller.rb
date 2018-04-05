5d050546bdcec4846a4a48605f7bfd72ba776c54
Jay Bednar <ironviralmedia@gmail.com>
2018-04-05 01:31:53 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,11 @@ require 'sinatra/base'
 # require '../config/environment.rb'
 
 class App < Sinatra::Base
-    set :root, File.sinatra-nested-forms-lab-superheroes-v-000('app/views')
+    
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-      erb :'super_hero'
+      erb :'..views/super_hero'
     end
 
     post'/teams' do
